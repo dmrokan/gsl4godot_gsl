8c9175bcc2a2ab33ca29b4bae19214a224420d99
Patrick Alken <alken@colorado.edu>
2016-09-08 12:01:36 -0600

    cleanup

--- multifit_nlinear/trust.c
+++ multifit_nlinear/trust.c
@@ -330,18 +330,6 @@ trust_iterate(void *vstate, const gsl_vector *swts,
           status = trust_eval_step(f, f_trial, g, J, dx, &rho, state);
           if (status == GSL_SUCCESS)
             foundstep = 1;
-
-#if 0 /*XXX*/
-          fprintf(stdout, "delta = %.12e |D dx| = %.12e |dx| = %.12e, dx0 = %.12e dx1 = %.12e |x_trial| = %.12e |f_trial| = %.12e rho = %.12e\n",
-                  state->delta,
-                  scaled_enorm(state->diag, dx),
-                  gsl_blas_dnrm2(dx),
-                  gsl_vector_get(dx, 0),
-                  gsl_vector_get(dx, 1),
-                  gsl_blas_dnrm2(x_trial),
-                  gsl_blas_dnrm2(f_trial),
-                  rho);
-#endif
         }
       else
         {
