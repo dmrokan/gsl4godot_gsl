d2c0d15e6a39a27a2ed6a8f644af1685e8eafd7d
Patrick Alken <alken@colorado.edu>
2017-02-26 18:36:49 -0700

    update license header

--- specfunc/test_hermite.c
+++ specfunc/test_hermite.c
@@ -1,26 +1,21 @@
-// test_hermite.c
-
-//*----------------------------------------------------------------------*
-//* This program is free software: you can redistribute it and/or modify *
-//* it under the terms of the GNU General Public License as published by *
-//* the Free Software Foundation, either version 3 of the License, or    *
-//* (at your option) any later version.                                  *
-//*                                                                      *
-//* This program is distributed in the hope that it will be useful,      *
-//* but WITHOUT ANY WARRANTY; without even the implied warranty of       *
-//* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
-//* GNU General Public License for more details.                         *
-//*                                                                      *
-//* You should have received a copy of the GNU General Public License    *
-//* along with this program. If not, see <http://www.gnu.org/licenses/>. *
-//*----------------------------------------------------------------------*
-
-//*----------------------------------------------------------------------*
-//* Test function for Hermite polynomials, functions etc.                *
-//* Copyright 2013-2014 Konrad Griessinger                               *
-//* (konradg(at)gmx.net)                                                 *
-//*----------------------------------------------------------------------*
-
+/* specfunc/test_hermite.c
+ * 
+ * Copyright (C) 2011, 2012, 2013, 2014 Konrad Griessinger
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
 
 #include <config.h>
 #include <gsl/gsl_math.h>
