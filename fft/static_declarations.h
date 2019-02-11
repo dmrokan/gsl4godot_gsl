6778ae02cb50a32de4fad5aaf560f17293a8cf95
Rhokan <okndemir@gmail.com>
2019-02-12 00:39:50 +0300

    GSL workarounds

---
+++ fft/static_declarations.h
@@ -0,0 +1,11 @@
+static int 
+FUNCTION(fft_complex,bitreverse_order) (BASE data[], 
+                                        const size_t stride,
+                                        const size_t n,
+                                        size_t logn);
+
+static int 
+FUNCTION(fft_real,bitreverse_order) (BASE data[], 
+                                     const size_t stride, 
+                                     const size_t n,
+                                     size_t logn);
